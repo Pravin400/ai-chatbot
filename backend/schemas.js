8d75da9ba88c474a1bf85ec3debf6b33d2562cd2
salman-itShaala <salman.shaikh@itshaala.com>
2025-03-15 12:03:01 +0530

    added user schema

---
+++ backend/schemas.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  userName: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: String,
+  createdAt: { type: Date, default: Date.now },
+  //   history : [{type : Date, qustion : String}]
+});
+
+export const User = mongoose.model("User", userSchema);
