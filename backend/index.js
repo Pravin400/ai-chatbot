44c0c78dc4bd806e80de542f870e4c53470c58b2
salman-itShaala <salman.shaikh@itshaala.com>
2025-03-15 11:22:44 +0530

    added functionality to get question from user and send its answer

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-//
-
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
@@ -12,13 +10,17 @@ const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+app.use(express.json());
+
 app.get("/", async (req, res) => {
+  const question = req.body.question;
+
   const chat = model.startChat({
     history: [],
   });
 
-  const result = await chat.sendMessage("What is html??");
-  const response = await result.response;
+  const result = await chat.sendMessage(question);
+  const response = result.response;
   const text = response.text();
 
   /** Send the response returned by the model as the API's response. */
